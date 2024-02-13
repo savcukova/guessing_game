ea38332d478de1321e4756368d4b6b8423d33548
Olina Savcukova <olinkasavcuk@gmail.com>
2024-02-13 17:41:37 +0100

    done

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 import random
+import os
 
 
 def guessing_game():
-    print("Welcome to the game 'Guess secret number'!")
-    print("I have one number on my mind between 1 and 100. \n")
-    
-    continue_game == "yes"
+    continue_game = "yes"
     while continue_game.lower() == "yes":
+        os.system("clear")
+        print("Welcome to the game 'Guess secret number'!")
+        print("I have one number on my mind between 1 and 100. \n")
         level = choose_difficulty_level()
         secret_number = generate_secret_number()
         user_guess(secret_number, level)
@@ -17,15 +18,11 @@ def guessing_game():
 
 def choose_difficulty_level():
     level = input("Choose level (write hard or easy): ").lower()
-    
-    if not level.isalpha:
-        print("Invalid input, enter a word 'hard' or 'easy'.")
-    else:
-        pass
+    if level not in ["hard", "easy"]:
+        print("Invalid input, enter 'hard' or 'easy'.")
     
     if level == "easy":
         print("you choose easy level. You have 10 attemps to guess number. ")
-        #dale zde bude funkce pro easy level 
     elif level == "hard":
         print("You choose  hard level. You have 5 attemps to guess number. ")
         
@@ -38,29 +35,34 @@ def generate_secret_number():
         
         
 def user_guess(secret_number, level):
-    if level == "easy":
-        attemps = 10
-    elif level == "hard":
-        attemps = 5
+    attempts = 10 if level == "easy" else 5
         
-    while attemps > 0:
-        guess = int(input("Guess the secret number: "))
+    while attempts > 0:
+        guess_input = input("Guess the secret number: ")
         
-        if guess == secret_number:
-            print(f"Congratulation, you guess the secret number {secret_number} correct! ")
-            break
-        elif guess > secret_number:
-            print("Too high number. Try again. ")
-        else:
-            print("Too low. Try again. ")
+        if guess_input.isdigit():
+            guess = int(guess_input)
             
-        attemps -= 1
-        print(f"Attemps left: {attemps}")
+            if 1 <= guess <= 100:
+                if guess == secret_number:
+                    print(f"Congratulations! You guessed the secret number {secret_number} correctly!")
+                    return
+                elif guess > secret_number:
+                    print("Too high. Try again.")
+                else:
+                    print("Too low. Try again.")
+            else:
+                print("Choose a number in the range of 1 and 100.")
+        else:
+            print("Please enter a valid number.")
+            
+        attempts -= 1
+        print(f"Attemps left: {attempts}")
 
-    if attemps == 0:
+    if attempts == 0:
         print("Sorry, you've run out of attemps. ")    
     
-    
+        
 def want_continue():
     continue_game = input("Do you want to start new game? 'yes'/'no': ")
     return continue_game    
